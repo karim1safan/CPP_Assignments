de85381e5c3800a95bb0ed9d7ceb3afe74d10ee6
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-22 21:47:07 +0200

    Create ass10.cpp

---
+++ Variables/ass10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x, y;
+
+  cout << "x = ";
+  cin >> x;
+
+  cout << "y = ";
+  cin >> y;
+
+  cout << "x * y = " << x * y << endl;
+  main(); // to repeat the question
+}
