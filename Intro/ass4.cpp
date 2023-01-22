0f6c1e8f057d2e86890b368ebd96b3ad2a024861
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-22 11:48:44 +0200

    Create ass4.cpp

---
+++ Intro/ass4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  std::out << "|=======================|\n";
+  std::out << "|== Elzero Web School ==|\n";
+  std::out << "|==        C++        ==|\n";
+  std::out << "|=======================|\n";
+
+  return 0;
+}
