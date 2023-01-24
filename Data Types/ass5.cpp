6ced154e737327f71c7e3da829585dd4129fc149
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-25 01:51:13 +0200

    Create ass5.cpp

---
+++ Data Types/ass5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a = '~';
+	char b = '&';
+	char c = '%';
+	char d = 'A';
+
+	cout << "ASCII Value of ~ Is " << int(a) << endl;
+	cout << "ASCII Value of ~ Is " << int(b) << endl;
+	cout << "ASCII Value of ~ Is " << int(c) << endl;
+	cout << "ASCII Value of ~ Is " << int(d) << endl;
+	// Output Needed
+	/*
+	"ASCII Value of ~ Is 126"
+	"ASCII Value of & Is 38"
+	"ASCII Value of % Is 37"
+	"ASCII Value of A Is 65"
+	*/
+	return 0;
+}
