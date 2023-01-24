a969976f6a433b7a9b0fa5ff161176821d91ce1f
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-25 01:57:18 +0200

    Create ass7.cpp

---
+++ Data Types/ass7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 1;
+	int b = 13;
+	int c = 17;
+	int d = 70;
+
+	cout << char(int(d - a));
+	cout << char(int(d + c));
+	cout << char(int(d + b));
+	// Output Needed
+	// "EWS"
+	return 0;
+}
