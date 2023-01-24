4e8380f7346dd6c7a835276a5ceb3acb80a606a1
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-25 01:47:11 +0200

    Create ass3.cpp

---
+++ Data Types/ass3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Maximum Integer Number Is => " << INT_MAX << endl;
+	cout << "Minimum Integer Number Is => " << INT_MIN << endl;
+
+	cout << "Maximum Integer Number Is => " << SHRT_MAX << endl;
+	cout << "Minimum Integer Number Is => " << SHRT_MIN << endl;
+	return 0;
+}
