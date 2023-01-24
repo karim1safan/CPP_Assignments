7f971386c749b321b4e88cdf4acafb7bb597b70a
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-25 01:37:10 +0200

    Create ass1.cpp

---
+++ Data Types/ass1.cpp
@@ -0,0 +1,15 @@
+// Example
+/*
+
+false				=> Boolean [bool]
+
+'a'					=> char 
+'9'					=> string
+9						=> integer [int]
+true				=> Boolean [bool]
+100					=> integer [int]
+-10					=> integer [int]
+0						=> integer [int]
+false				=> Boolean [bool]
+10.9				=> float or double
+*/
