90974f202592bd3c4880d186dd4eb0f8ebf4cf9a
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-25 01:54:18 +0200

    Create ass6.cpp

---
+++ Data Types/ass6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 69;
+	int b = 108;
+	int c = 122;
+	int d = 101;
+	int e = 114;
+	int f = 111;
+
+	cout << char(a); 
+	cout << char(b); 
+	cout << char(c);
+	cout << char(d);
+	cout << char(e);
+	cout << char(f) << endl;
+
+	// Output Needed
+	/*
+	"Character Of This ASCII Value 69 Is E"
+	"Character Of This ASCII Value 108 Is l"
+	"Character Of This ASCII Value 122 Is z"
+	"Character Of This ASCII Value 101 Is e"
+	"Character Of This ASCII Value 114 Is r"
+	"Character Of This ASCII Value 111 Is o"
+	*/
+
+	// Output Needed In Last Line
+	//"Elzero"
+	return 0;
+}
