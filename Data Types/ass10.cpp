6670710b348b59c064b457faa313e01c014c63b2
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-01-25 02:09:11 +0200

    Create ass10.cpp

---
+++ Data Types/ass10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Add Type Alias Here
+
+	// Do Not Edit
+	long long int num = 150050005; // This Is Long Long Int
+	cout << num << "\n"; // 150050005
+	return 0;
+}
